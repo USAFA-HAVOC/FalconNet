a470fd53a6de4f404dd35d8555f8e5e0c38e50a6
anderryl <rylie@4andersons.com>
2023-09-20 10:08:59 -0600

    Fixed Demo

--- lib/Model/Store/DemoData.dart
+++ lib/Model/Store/DemoData.dart
@@ -1,13 +1,11 @@
 import 'package:built_collection/built_collection.dart';
 import 'package:falcon_net/Model/Database/AccountabilityEvent.dart';
-import 'package:falcon_net/Model/Database/FormSummary.dart';
 import 'package:falcon_net/Model/Database/InitialData.dart';
 import 'package:falcon_net/Model/Database/UserEvent.dart';
 import 'package:falcon_net/Model/Database/UserStatus.dart';
 
 import '../Database/CadetAccountability.dart';
 import '../Database/CadetPass.dart';
-import '../Database/FormList.dart';
 import '../Database/TimedRole.dart';
 import '../Database/User.dart';
 import '../Database/UserGrades.dart';
@@ -85,30 +83,26 @@ final defaultGrades = UserGrades((u) => u
   ].build().toBuilder()
 );
 
-final defaultForms = FormList((f) => f
-    ..forms = <FormSummary>[
-      FormSummary((f) => f
-        ..title = "Demo Form"
-        ..description = "This is a signable document, which by some accounts, may be signed."
-        ..form_id = "demo1"
-        ..signed = false
-      ),
-      FormSummary((f) => f
-        ..title = "Demo Form #2"
-        ..description = "I think you get the point."
-        ..form_id = "demo2"
-        ..signed = false
-      ),
-    ].build().toBuilder()
-);
-
 final defaultEvents = ListBuilder<UserEvent>([
   UserEvent((e) => e
       ..event_id = "demo_di_id"
       ..type = EventType.di.name
       ..accountability_method = AccountabilityMethod.di.name
-      ..time = DateTime.now().add(Duration(hours: 3))
+      ..time = DateTime.now().add(const Duration(hours: 3))
       ..status = UserStatus.unsigned.name
+      ..submission_deadline = DateTime.now().add(const Duration(hours: 4))
+      ..submission_start = DateTime.now().subtract(const Duration(hours: 1))
+  ),
+
+  UserEvent((e) => e
+    ..event_id = "demo_event"
+    ..type = EventType.other.name
+    ..name = "Test Event"
+    ..accountability_method = AccountabilityMethod.self_signed.name
+    ..time = DateTime.now().add(const Duration(hours: 3))
+    ..status = UserStatus.unsigned.name
+    ..submission_deadline = DateTime.now().add(const Duration(hours: 4))
+    ..submission_start = DateTime.now().subtract(const Duration(hours: 1))
   )
 ]).build();
 
@@ -117,6 +111,8 @@ final InitialData initial = InitialData((i) => i
     ..pass_history = defaultHistory
     ..grades = defaultGrades.toBuilder()
     ..events = defaultEvents.toBuilder()
+    ..event_excusals = ListBuilder()
+    ..recurring_excusals = ListBuilder()
 );
 
 final demoEndpoints = <String, dynamic Function(dynamic)>{
@@ -124,7 +120,6 @@ final demoEndpoints = <String, dynamic Function(dynamic)>{
   "/profile/info" : (_) => defaultUser,
   "/passes/history" : (_) => defaultHistory,
   "/grades/info" : (_) => defaultGrades,
-  "/forms/info" : (_) => defaultForms,
   "/passes/create" : (pass) => pass,
   "/leave/create" : (leave) => leave,
   "/profile/edit" : (_) => true,
